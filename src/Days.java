2b7f21ca4cd7934f30e4539a3a1963f412fc2f4f
csbsantos <claudiasbsantos13@gmail.com>
2025-09-17 19:36:05 +0100

    Update Days.java

--- src/Days.java
+++ src/Days.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+public enum Days {MONDAY(1, true),
+    TUESDAY(2, true),
+    WEDNESDAY(3, true),
+    THURSDAY(4, true),
+    FRIDAY (5, true),
+    SATURDAY (6, false),
+    SUNDAY (7, false);
 
-public class Days {
+
+    private int number;
+    private boolean weekday;
+
+    Days(int number, boolean weekday) {
+        this.number = number;
+        this.weekday= weekday;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean isWeekday() {
+        return weekday;
+    }
 }
